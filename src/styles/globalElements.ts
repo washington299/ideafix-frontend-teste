5bcca00c4000e01b35933ecb1b288f954e0c4a45
Washington Campos <washingtoncampos9@gmail.com>
2021-08-04 13:18:18 -0300

    feat: create cardShadow style component

--- src/styles/globalElements.ts
+++ src/styles/globalElements.ts
@@ -6,3 +6,14 @@ export const title = css`
 	font-weight: var(--font-weight-bold);
 	color: var(--purple);
 `;
+
+export const cardShadow = css`
+	padding: var(--size-small) var(--size-medium);
+	background-color: var(--light-grey);
+	color: var(--grey);
+	font-weight: var(--font-weight-bold);
+	box-shadow: 0 2px 2px 0 var(--grey);
+
+	border: none;
+	border-radius: 10px;
+`;
