11fc84ec2037c6280f2b6f7b1120dd399810b578
Washington Campos <washingtoncampos9@gmail.com>
2021-08-04 15:22:07 -0300

    feat: create Input, Textarea and Button components

--- src/components/BlockOfNotes/index.tsx
+++ src/components/BlockOfNotes/index.tsx
@@ -4,6 +4,13 @@ const BlockOfNotes = () => {
 	return (
 		<S.Wrapper>
 			<S.Title>Bloco de notas</S.Title>
+
+			<S.Input placeholder="Assunto" />
+			<S.Textarea placeholder="Texto" />
+
+			<S.ButtonWrapper>
+				<S.Button>Criar nota</S.Button>
+			</S.ButtonWrapper>
 		</S.Wrapper>
 	);
 };
--- src/components/BlockOfNotes/styles.ts
+++ src/components/BlockOfNotes/styles.ts
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 
-import { title } from "styles/globalElements";
+import { title, cardShadow } from "styles/globalElements";
 
 export const Wrapper = styled.section`
 	width: 500px;
@@ -13,4 +13,39 @@ export const Title = styled.h1`
 	${title};
 
 	font-size: var(--size-large);
+`;
+
+export const Input = styled.input`
+	${cardShadow};
+
+	width: 100%;
+	margin-bottom: var(--size-small);
+	outline: none;
+`;
+
+export const Textarea = styled.textarea`
+	${cardShadow};
+
+	width: 100%;
+	height: 200px;
+	margin-bottom: var(--size-small);
+	font-size: var(--size-small);
+	outline: none;
+	resize: none;
+`;
+
+export const ButtonWrapper = styled.div`
+	display: flex;
+	justify-content: flex-end;
+`;
+
+export const Button = styled.button`
+	padding: var(--size-extra-small) var(--size-small);
+	background-color: var(--orange);
+	color: var(--white);
+
+	border: none;
+	border-radius: 5px;
+	text-transform: uppercase;
+	cursor: pointer;
 `;
