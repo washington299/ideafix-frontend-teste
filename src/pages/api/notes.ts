cd276c3d751bc5e8b25fa1000bbfb1432d4b19b5
Washington Campos <washingtoncampos9@gmail.com>
2021-08-05 10:42:32 -0300

    feat: create query createNote

--- src/pages/api/notes.ts
+++ src/pages/api/notes.ts
@@ -2,6 +2,12 @@ import type { NextApiRequest, NextApiResponse } from "next";
 
 import mockNotes from "mock/notes.json";
 
-export default function (_req: NextApiRequest, res: NextApiResponse) {
-	res.status(200).json(mockNotes);
+export default function (req: NextApiRequest, res: NextApiResponse) {
+	if (req.method === "GET") {
+		res.status(200).json(mockNotes);
+	}
+
+	if (req.method === "POST") {
+		res.status(200).json({ msg: "Nota criada com sucesso!" });
+	}
 }
--- src/services/queries.ts
+++ src/services/queries.ts
@@ -2,4 +2,5 @@ import { api } from "services/config";
 
 export const queries = {
 	getNotes: () => api.get("/notes"),
+	createNote: () => api.post("/notes"),
 };
